08527c836f44bde9b29f289cbdabb522ff793026
Junki Yonezu <yonegen0@gmail.com>
2025-05-20 22:58:36 +0900

    [add]タスク追加API呼び出し追加

--- src/components/todo_list.js
+++ src/components/todo_list.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
@@ -10,6 +10,9 @@ import Checkbox from '@mui/material/Checkbox';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
+
+// バックエンドのbaseurl
+const WEB_API_URL = 'http://127.0.0.1:5000';
 
 // スタイルを適用したPaperコンポーネントを作成
 const StyledPaper = styled(Paper)(({ theme }) => ({
@@ -27,7 +30,62 @@ function TodoApp() {
   const [tasks, setTasks] = useState([]);
   // 新しいTodoアイテムの入力値を管理するstate
   const [newTask, setNewTask] = useState('');
+  
+  // /get_user を呼び出す処理
+  const callGetUserApi = useCallback(() => {
+    fetch(`${WEB_API_URL}/get_user`, {
+      mode: 'cors',
+      method: 'GET'
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response)
+        }
+        return response.json(); // レスポンスが OK なら JSON を解析して次の .then に渡す
+      })
+      .then(data => {
+        // 予定の配列 (tasks) を FullCalendar が認識できるイベントオブジェクトの配列に変換
+        const prev_tasks = Object.entries(data).map(([id, task])=> ({
+          id:id,
+          // イベントのタイトルは予定のテキスト
+          text: task.task_text,
+          // イベントの開始日は予定の日付
+          completed: task.completed
+        }));
+        if (prev_tasks) {
+          setTasks(...tasks, prev_tasks); // 取得したdataを tasks state に設定
+        }
+      })
+      .catch(e => {
+        console.error('Error calling:', e);
+      });
+    }, [WEB_API_URL]);
+  
+  useEffect(() => {
+    // データベースの情報取得
+    callGetUserApi();
+  }, [callGetUserApi]);
 
+  // /settask を呼び出す処理
+  const callSetTaskApi = useCallback(() => {
+    fetch(`${WEB_API_URL}/settask`, {
+      mode: 'cors',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify([{ text: newTask }]),
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response)
+        }
+        return 
+      })
+      .catch(e => {
+        console.error('Error calling:', e);
+      });
+    }, [newTask]);
   // テキスト入力フィールドの値が変更用関数
   const handleInputChange = (event) => {
     setNewTask(event.target.value); // 入力された新しい値をnewTaskに設定
@@ -38,8 +96,9 @@ function TodoApp() {
     // 入力されたタスクが空でなければ
     if (newTask.trim() !== '') {
       // tasks配列に新しいタスクオブジェクトを追加
-      setTasks([...tasks, { id: Date.now(), text: newTask, completed: false }]);
+      callSetTaskApi();
       setNewTask(''); // 入力フィールドをクリア
+      callGetUserApi()
     }
   };
 
